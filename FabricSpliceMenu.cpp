aba4726ebfc7696b8dbaefb4aa3a095c6ef238b5
Eric Mootz <eric@mootzoid.com>
2015-05-13 16:41:38 +0200

    fixed links to help web pages

--- FabricSpliceMenu.cpp
+++ FabricSpliceMenu.cpp
@@ -121,7 +121,7 @@ SICALLBACK FabricSplice_Menu_OnlineHelp( XSI::CRef& )
 {
   CValue returnVal;
   CValueArray args(3);
-  args[0] = "http://documentation.fabricengine.com/Splice/latest/HTML/";
+  args[0] = "http://docs.fabric-engine.com/FabricEngine/latest/HTML/Splice/Softimage/index.html";
   args[1] = true;
   args[2] = (LONG)1l;
   Application().ExecuteCommand(L"OpenNetView", args, returnVal);
@@ -132,7 +132,7 @@ SICALLBACK FabricSplice_Menu_ThirdPartyLicenses( XSI::CRef& )
 {
   CValue returnVal;
   CValueArray args(3);
-  args[0] = "http://documentation.fabricengine.com/Splice/latest/HTML/thirdpartylicenses.html";
+  args[0] = "http://docs.fabric-engine.com/FabricEngine/latest/HTML/Splice/thirdpartylicenses.html";
   args[1] = true;
   args[2] = (LONG)1l;
   Application().ExecuteCommand(L"OpenNetView", args, returnVal);
